3e82d7d4c96d3f3277a310d178d1ad8645ee9808
Jorge Useche <naturalmentejorge@gmail.com>
2017-02-20 12:04:46 -0500

    Ajustes para la entrega 1 a Acuecto. Minificado exitoso.

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-#Visor de datos geográficos del acueducto
+# Visor de datos geográficos del acueducto
 
-##Requerimientos:
+## Requerimientos:
 -----------------
 
 - OpenLayers v3: Biblioteca de Javascript para Mapas en la Web
 - Gulp: Systema de Construcción y Automatización de Flujo de Trabajo
 - Bower: Gestor de Paquetes Web
 - NPM: Gestor de Paquetes de NodeJS, gestiona también bibliotecas JS del lado del Cliente
+- Proxy arcgis: Permite hacer túnel de las peticiones más grandes que las de GET
+que son alrrededor de 2000 caracteres (2048).
 
-##Utiliza recomendaciones de estilo estándar:
+## Utiliza recomendaciones de estilo estándar:
 
 - StandardJS: http://standardjs.com/rules.html
 - JSDoc: http://usejsdoc.org/about-getting-started.html
 
-##Instalación
+## Instalación
 
 Solo se necesita ejecutar el script llamado *install*
 
 ```bash
 $ ./install
 ```
-##Ejecutar
+## Ejecutar
 
 Solo se necesita ejecutar el script llamado *run*
 
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@
 var gulp = require('gulp')
 var webserver = require('gulp-webserver')
 var minify = require('gulp-minifier')
+var babel = require('gulp-babel')
+var merge = require('merge-stream')
 
 gulp.task('webserver', function() {
     gulp.src('app')
@@ -28,23 +30,50 @@ gulp.task('webserver', function() {
 })
 
 /*
-//https://www.youtube.com/watch?v=TGiBG6II6Jk
+// https://www.youtube.com/watch?v=TGiBG6II6Jk
 gulp.task('watch', ['sass'], function (){
   gulp.watch('./sass/**\/*.css', ['sass'])
 });
 */
 gulp.task('minify', function() {
-  return gulp.src('app/**/*').pipe(minify({
-    minify: true,
-    collapseWhitespace: true,
-    conservativeCollapse: true,
-    minifyJS: true,
-    minifyCSS: true,
-    getKeptComment: function (content, filePath) {
-        var m = content.match(/\/\*![\s\S]*?\*\//img);
-        return m && m.join('\n') + '\n' || '';
-    }
-  })).pipe(gulp.dest('dist/app'));
+    var allfiles = gulp.src(['app/**/*', '!app/vendor/**/*', '!app/**/*.js'])
+        .pipe(minify({
+            minify: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            minifyJS: true,
+            minifyCSS: true,
+            getKeptComment: function(content, filePath) {
+                var m = content.match(/\/\*![\s\S]*?\*\//img);
+                return m && m.join('\n') + '\n' || '';
+            }
+        }))
+        .pipe(gulp.dest('dist/app'))
+    // http://stackoverflow.com/questions/34398338/uglification-failed-unexpected-character
+    var jsfiles = gulp.src(['app/**/*.js', '!app/vendor/**/*', '!app/js/mustache-dojo.js'])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(minify({
+            minify: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            minifyJS: true,
+            minifyCSS: true,
+            getKeptComment: function(content, filePath) {
+                var m = content.match(/\/\*![\s\S]*?\*\//img);
+                return m && m.join('\n') + '\n' || '';
+            }
+        }))
+        .pipe(gulp.dest('dist/app'))
+    var vendorfiles = gulp.src(['app/vendor/**/*'])
+        .pipe(gulp.dest('dist/app/vendor'))
+
+    var exeptionfiles = gulp.src(['app/js/mustache-dojo.js'])
+        .pipe(gulp.dest('dist/app/js'))
+
+    // https://github.com/gulpjs/gulp/blob/master/docs/recipes/using-multiple-sources-in-one-task.md
+    return merge(allfiles, jsfiles, vendorfiles, exeptionfiles)
 })
 
 //https://github.com/andresvia/go-angular-drone/blob/master/.drone.yml
